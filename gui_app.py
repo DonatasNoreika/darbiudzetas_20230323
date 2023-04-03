e47c8ad2d73a508b2ddce02680a89b7bb1b0541a
Donatas Noreika <donoras@gmail.com>
2023-04-03 09:33:04 +0300

    Pridėtas listbox su visais įrašais

--- gui_app.py
+++ gui_app.py
@@ -1,11 +1,15 @@
 
-from tkinter import Tk, Label, Entry, Button, END
+from tkinter import Tk, Label, Entry, Button, END, Listbox
 
 from biudzetas import Biudzetas
 
 langas = Tk()
 
 biudzetas = Biudzetas()
+
+def atnaujinti_listbox():
+    boksas_listbox.delete(0, END)
+    boksas_listbox.insert(END, *biudzetas.zurnalas)
 
 def ivesti():
     suma = float(suma_entry.get())
@@ -14,14 +18,17 @@ def ivesti():
     else:
         biudzetas.prideti_islaidu_irasa(abs(suma), "", "")
     suma_entry.delete(0, END)
+    atnaujinti_listbox()
 
 langas.geometry("250x200")
 suma_label = Label(langas, text="Įrašykite sumą")
 suma_entry = Entry(langas)
 suma_button = Button(langas, text="Įvesti", command=ivesti)
-
+boksas_listbox = Listbox(langas)
+boksas_listbox.insert(END, *biudzetas.zurnalas)
 
 suma_label.grid(row=0, column=0)
 suma_entry.grid(row=0, column=1)
 suma_button.grid(row=0, column=2)
+boksas_listbox.grid(row=1, columnspan=3)
 langas.mainloop()
